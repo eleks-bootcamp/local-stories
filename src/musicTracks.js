1aad4637842308c7afd75461cd98d5d5f7142bf4
volodymyr35 <vzhukevych@gmail.com>
2022-08-22 22:43:54 +0300

    created audioplayer and added new files to it

---
+++ src/musicTracks.js
@@ -0,0 +1,73 @@
+const musicTracks = [
+  {
+      title: "Action",
+      artist: "Stylish-rock",
+     audioSrc: require("../src/audioFiles/action-rock.mp3")
+  },
+
+  {
+      title: "Adventure",
+      artist: "Bensound",
+      audioSrc: require("../src/audioFiles/bensound-adventure.mp3")
+  },
+
+  {
+      title: "Corporate",
+      artist: "Business",
+      audioSrc: require("../src/audioFiles/corporate-business.mp3")
+  },
+
+  {
+      title: "Electronic",
+      artist: "Rock-king",
+      audioSrc: require("../src/audioFiles/electronic-rock-king.mp3")
+  },
+
+  {
+      title: "Epic",
+      artist: "Cinematic",
+      audioSrc: require("../src/audioFiles/epic-cinematic.mp3")
+  },
+
+  {
+      title: "Happy",
+      artist: "Day",
+      audioSrc: require("../src/audioFiles/happy-day.mp3")
+  },
+
+  {
+      title: "Leonell",
+      artist: "Cassio",
+      audioSrc: require("../src/audioFiles/leonell-cassio.mp3")
+  },
+
+  {
+      title: "Tension",
+      artist: "Tension",
+      audioSrc: require("../src/audioFiles/tension.mp3")
+  },
+
+  {
+      title: "Trendy",
+      artist: "Summer",
+      audioSrc: require("../src/audioFiles/trendy-summer.mp3")
+  },
+
+  {
+      title: "Tropical",
+      artist: "Summer",
+      audioSrc: require("../src/audioFiles/tropical-summer.mp3")
+  }
+  {
+      title: "About-fontans-1",
+      artist: "Noname",
+      audioSrc: require("../src/audioFiles/about-fontans-1.wav")
+  }
+  {
+      title: "About-fontans-2",
+      artist: "Yarema",
+      audioSrc: require("../src/audioFiles/about-fontans-2.wav")
+  }
+];
+
+export default musicTracks;
