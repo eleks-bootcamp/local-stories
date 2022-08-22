d43843457dd576cf226e73de6c82a83315530c7f
Yarema <yaremaphoto@gmail.com>
2022-08-22 21:43:03 +0300

    init add test

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import AudioPlayerVeaw from './components/AudioPlayerVeaw';
+import GeoLocationVeaw from './components/GeoLocationVeaw';
+
+function App() {
+  return (
+    <div className="App">
+      App
+      <AudioPlayerVeaw />
+      <GeoLocationVeaw />
+    </div>
+  );
+}
+
+export default App;
