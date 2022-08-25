57bb579101d526ef4ac3af7bf0c6537af068a2e3
volodymyr35 <vzhukevych@gmail.com>
2022-08-25 12:08:11 +0300

    added new audio-file

--- src/musicTracks.js
+++ src/musicTracks.js
@@ -9,6 +9,12 @@ const musicTracks = [
       title: "About-fontans-2",
       artist: "Yarema",
       audioSrc: require("../src/audioFiles/about-fontans-2.wav")
+  },
+ 
+ {
+      title: "Market-square",
+      artist: "Somebody",
+      audioSrc: require("../src/audioFiles/market-square.wav")
   }
 ];
 
