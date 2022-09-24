a65ee1d5a014d5feae8bc0063dba456ed33af8e0
Yarema <yaremaphoto@gmail.com>
2022-09-25 00:42:31 +0300

    husky, pre-commit, pre-push

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import './App.css';
-import AudioPlayerVeaw from './components/AudioPlayerVeaw';
-import GeoLocationVeaw from './components/GeoLocationVeaw';
+import './App.css'
+import React from 'react'
+import AudioPlayerVeaw from './components/AudioPlayerVeaw'
+import GeoLocationVeaw from './components/GeoLocationVeaw'
 
-function App() {
-  console.log('knknnknk');
+function App () {
+  // console.log('knknnknk')
   return (
     <div className="App">
       App
       <AudioPlayerVeaw />
       <GeoLocationVeaw />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
